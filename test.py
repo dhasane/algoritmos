be5592db2a8233d6f76e0d2a3c890cf483e0bc3c
gamendez98 <36478111+gamendez98@users.noreply.github.com>
2019-04-13 11:41:51 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,38 @@
+import os
+import threading
+
+def crearJugador(arg):#retorna la funcion del jugador y el nombre del jugador
+    command=""
+    name=""
+    if arg[len(arg)-3:]==".py":
+        name=arg[:len(arg)-3]
+        command="python "+arg
+        pipename="pipe"+name
+    elif arg[len(arg)-4:]==".jar":
+        name=arg[:len(arg)-4]
+        command="java -jar "+arg
+        pipename="pipe"+name
+    else:
+        name=arg
+        command="./"+arg
+        pipename="pipe"+arg
+    os.mkfifo(pipename+'T')
+    os.mkfifo(pipename+'M')
+    print command
+    thr = threading.Thread(target=lambda :os.system(command) )
+    thr.start()
+
+    def playerPipe(E,J):
+        pipeT=open(pipename+'T','w')
+        pipeM=open(pipename+'M','r')
+        pipeT.write(E)
+        pipeT.close()
+        return pipeM.readline()
+    return playerPipe
+
+pl=crearJugador('fifo')
+
+mensaje=''
+while(True):
+    mensaje=raw_input(':')
+    print pl(mensaje,'x')
